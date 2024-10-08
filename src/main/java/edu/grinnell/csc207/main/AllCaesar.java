42c31e564e52432a6ae7af3969f4296b827a8ccb
NICOLE_GORRELL <GORRELLN@grinnell.edu>
2024-10-08 11:52:14 -0500

    began adding documentation to AllCaesar.java

--- src/main/java/edu/grinnell/csc207/main/AllCaesar.java
+++ src/main/java/edu/grinnell/csc207/main/AllCaesar.java
@@ -7,28 +7,35 @@ import java.lang.System;
 
 import edu.grinnell.csc207.util.CipherUtils;
 
-@SuppressWarnings("unused")
+//@SuppressWarnings("unused")
+/**
+ * Generate all 26 possible encoded/decoded (via the Caesar method) versions of a message.
+ * 
+ * @author Nicole Gorrell
+ */
 public class AllCaesar {
   public static void main(String[] args) {
     char[] inTwo = args[1].toCharArray();
 
+    // Checks for correct amount of command line inputs.  
     if (args.length != 2) {
       System.err.println("Error: Incorrect number of parameters.");
-    } 
+      return; // End program.
+    } // if 
     
+    // Ensures that the first command argument is a valid input
     if ((!(args[0].equals("encode"))) && (!(args[0].equals("decode"))) ) {
       System.err.println("Error: Invalid option: " + args[0] + ". Valid options are 'encode' or 'decode'.");
+      return; // End program
+    } // if
 
-      return;
-    }
-
+    // Ensures the input message to process is in a valid format
     for (char index : inTwo) {
-      if (!(Character.isAlphabetic(index))) {
+      if (!(Character.isAlphabetic(index))) { // doesn't necessarily check for lowercase...
         System.err.println("Error: String contains characters other than lowercase letters.");
-
-        return;
-      }
-    }
+        return; // End program
+      } // if
+    } // for
 
     PrintWriter pen = new PrintWriter(System.out, true);
     String str = args[1];
@@ -37,5 +44,5 @@ public class AllCaesar {
     }
     pen.flush();
     pen.close();
-  }
-} //class AllCaesar
+  } // main(String[])
+} // class AllCaesar
